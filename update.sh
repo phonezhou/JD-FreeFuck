80fe56f64fb5028d7e3c27459f72df7b4170c469
phonezhou <47355457+phonezhou@users.noreply.github.com>
2021-04-24 20:39:10 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/env bash
-## Author:SuperManito
+## Author:phonezhou
 ## Date:2021-3-
 
 ## 项目安装目录
@@ -16,7 +16,7 @@ echo -e "\033[32m | 特别提示：后续使用遇到问题请访问本人项目
 echo -e "\033[32m |                                                             | \033[0m"
 echo -e "\033[32m |           我不是活动脚本的开发者，所有活动问题与我无关      | \033[0m"
 echo -e "\033[32m |                                                             | \033[0m"
-echo -e "\033[32m | 项目地址：https://github.com/SuperManito/JD-FreeFuck        | \033[0m"
-echo -e "\033[32m |           https://gitee.com/SuperManito/JD-FreeFuck         | \033[0m"
+echo -e "\033[32m | 项目地址：https://github.com/phonezhou/JD-FreeFuck        | \033[0m"
+echo -e "\033[32m |           https://gitee.com/phonezhou/JD-FreeFuck         | \033[0m"
 echo -e "\033[32m |                                                             | \033[0m"
 echo -e "\033[32m +-------------------------------------------------------------+ \033[0m"
